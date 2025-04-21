8ac93ccf3a7f53378ba274c2f4d99532d7739359
Leslie Annan <kenannan005@gmail.com>
2025-04-21 10:17:35 +0000

    Initial Commit

---
+++ capture/captureApp.swift
@@ -0,0 +1,17 @@
+//
+//  captureApp.swift
+//  capture
+//
+//  Created by Leslie Annan on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct captureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
